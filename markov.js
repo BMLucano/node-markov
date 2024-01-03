6f1e8338778a010e1d3ee17fefd0ee99d87ff6ea
Brandie Lucano <blucano.dev@gmail.com>
2024-01-02 16:03:11 -0800

    get chains and get text function need to be tested

--- markov.js
+++ markov.js
@@ -15,7 +15,7 @@ class MarkovMachine {
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +23,30 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
     // TODO: implement this!
+
+    //create object
+    //loop through words
+    //get each word, add as a key to the object
+      // next word will be a value of that key or null if no next word
+
+      const chains = {}
+
+      for(let i = 0; i < this.words.length; i++){
+        let word = this.words[i];
+        let nextWord = this.words[i+1] || null;
+
+        if(word in chains){
+          chains[word].push(nextWord);
+        }else{
+          chains[word] = [nextWord];
+        }
+      }
+      return chains;
   }
 
 
@@ -40,5 +59,18 @@ class MarkovMachine {
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+    let randomText = "";
+    let currWord = this.words[0]
+
+    //while following word is not null
+      //pick random index of array
+      //add to an array of new text
+    while(currWord !== null){
+      let randomIdx = Math.floor(Math.random() * this.chains[currWord].length - 1);
+      let randomWord = this.chains[currWord][randomIdx];
+      randomText += randomWord;
+      currWord = randomWord;
+    }
+    return randomText;
   }
 }
